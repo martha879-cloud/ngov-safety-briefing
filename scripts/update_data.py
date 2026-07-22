c7c491da46b0876eb03c93619bf0883931397e1b
martha879-cloud <martha879@gmail.com>
2026-07-22 15:39:03 +0900

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -8,6 +8,29 @@ API_KEY = os.getenv("MOFA_API_KEY")
 
 URL = "https://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2"
 
+TARGET_COUNTRIES = {
+"동티모르": "asia",
+"라오스": "asia",
+"몽골": "asia",
+"방글라데시": "asia",
+"베트남": "asia",
+"인도네시아": "asia",
+"캄보디아": "asia",
+"필리핀": "asia",
+"르완다": "africa",
+"말라위": "africa",
+"모로코": "africa",
+"우간다": "africa",
+"이집트": "africa",
+"케냐": "africa",
+"탄자니아": "africa",
+"과테말라": "latin",
+"도미니카공화국": "latin",
+"페루": "latin",
+"요르단": "middle",
+"키르기스스탄": "middle"
+}
+
 params = {
 "serviceKey": API_KEY,
 "returnType": "JSON",
